358692bd0c1d45a0e2ed9be0932b364220a68d4d
Gryzvwald <gryzvwald@gmail.com>
2018-08-25 14:15:35 -0700

    Version: 7.0.18726, gold data updates.

--- Guides/Achievements/ZygorAchievementsHordeBFATrial.lua
+++ Guides/Achievements/ZygorAchievementsHordeBFATrial.lua
@@ -3,3 +3,4 @@ if not ZygorGuidesViewer then return end
 if UnitFactionGroup("player")~="Horde" then return end
 if ZGV:DoMutex("AchievementsHBFA") then return end
 ZygorGuidesViewer.GuideMenuTier = "TRI"
+ZGV.BETAEND()
--- Ver.lua
+++ Ver.lua
@@ -1,7 +1,7 @@
 assert(ZygorGuidesViewer,"Zygor Guides Viewer failed to load.")
 local name,_=...
-ZygorGuidesViewer.revision = tonumber(string.sub("$Revision: 18690 $", 12, -3))
+ZygorGuidesViewer.revision = tonumber(string.sub("$Revision: 18726 $", 12, -3))
 ZygorGuidesViewer.version = GetAddOnMetadata(name,"version") .. "." .. ZygorGuidesViewer.revision
-ZygorGuidesViewer.date = string.sub("$Date: 2018-08-20 12:55:17 -0400 (Mon, 20 Aug 2018) $", 8, 17)
---2018/08/20 09:55:04
+ZygorGuidesViewer.date = string.sub("$Date: 2018-08-24 05:55:56 -0400 (Fri, 24 Aug 2018) $", 8, 17)
+--2018/08/24 02:55:49
  
